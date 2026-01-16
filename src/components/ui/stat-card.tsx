2df409a6c90a01d490ec34305775db02314a1e44
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2026-01-16 16:59:53 +0530

    feat: enhance StatCard component with navigation support and improved styling

--- src/components/ui/stat-card.tsx
+++ src/components/ui/stat-card.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import { useNavigate } from "react-router";
 
 type StatCardProps = {
   title: string;
   value: React.ReactNode;
   icon?: React.ReactNode;
-  // `color` may be a Tailwind background class like 'bg-blue-600' or a CSS color string like '#1e40af'
   color?: string;
   className?: string;
+  navigateTo?: string;
 };
 
 export default function StatCard({
@@ -17,18 +18,27 @@ export default function StatCard({
   icon,
   color,
   className,
+  navigateTo,
 }: StatCardProps) {
+  const navigation = useNavigate();
   return (
     <Card
-      className={cn("p-5 rounded-md text-white border-none", className, color)}
+      className={cn(
+        "sm:p-5 px-3 py-2 rounded-md text-white border-none cursor-pointer",
+        className,
+        color
+      )}
+      onClick={() => navigateTo && navigation(navigateTo)}
     >
       <div className="flex items-center justify-between">
         <div>
-          <p className="text-sm opacity-90">{title}</p>
-          <p className="text-xl md:text-2xl mt-1">{value}</p>
+          <p className="md:text-base text-xs opacity-90">{title}</p>
+          <p className="md:text-2xl text-base mt-1 w-[70px] sm:w-auto overflow-y-hidden overflow-x-auto">
+            {value}
+          </p>
         </div>
 
-        <div className="shrink-0 bg-white p-2 rounded-md">{icon}</div>
+        <div className="bg-white sm:p-2 p-1 rounded-md">{icon}</div>
       </div>
     </Card>
   );
